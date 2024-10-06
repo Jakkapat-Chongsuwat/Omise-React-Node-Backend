96d2fc301ff0d0beac0cd95d9ba53f35ffe3b17e
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2024-10-06 23:40:01 +0700

    [Add]: create charge fn

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+var omise = require("omise")({
+  publicKey: process.env.OMISE_PUBLIC_KEY,
+  secretKey: process.env.OMISE_SECRET_KEY,
+});
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/checkout-credt-card", async (req, res, next) => {
+  console.log(req.body);
+
+  // createCharge();
+});
+
+const createCharge = async () => {
+  try {
+    const customer = await omise.customers.create({
+      email: "john.doe@example.com",
+      description: "John Doe (id: 30)",
+      card: "tokn_test_61bp0ne4sijy7mwncli",
+    });
+
+    const charge = await omise.charges.create({
+      amount: 100000,
+      currency: "thb",
+      customer: customer.id,
+    });
+
+    console.log(charge);
+
+    next();
+  } catch (error) {
+    console.error("Error creating charge:", error);
+  }
+};
 
 app.listen(80, () => {
-  console.log('Server is up')
-})
+  console.log("Server is up");
+});
