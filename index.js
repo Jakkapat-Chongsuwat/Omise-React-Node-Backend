83c5b99ccb157eb3828b2c9ba611473a9950581a
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2024-10-08 00:11:30 +0700

    [Add]: res body and req body to create charge

--- index.js
+++ index.js
@@ -14,31 +14,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/checkout-credt-card", async (req, res, next) => {
+app.post("/checkout-credit-card", async (req, res, next) => {
   console.log(req.body);
 
-  // createCharge();
+  await createCharge(req.body, res, next);
 });
 
-const createCharge = async () => {
+const createCharge = async (body, res, next) => {
+  const { email, name, amount, token } = body;
   try {
     const customer = await omise.customers.create({
-      email: "john.doe@example.com",
-      description: "John Doe (id: 30)",
-      card: "tokn_test_61bp0ne4sijy7mwncli",
+      email,
+      description: name,
+      card: token,
     });
 
     const charge = await omise.charges.create({
-      amount: 100000,
+      amount,
       currency: "thb",
       customer: customer.id,
     });
 
     console.log(charge);
 
+    res.status(200).json({
+      message: "Charge created successfully",
+      amount: charge.amount,
+      status: charge.status,
+    });
+
     next();
   } catch (error) {
     console.error("Error creating charge:", error);
+
+    res.status(500).json({
+      message: "Error creating charge",
+      error: error.message,
+    });
   }
 };
 
